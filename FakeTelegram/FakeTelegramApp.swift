e517fb362ed9065cf83d9a1bfc090f90d601f085
Snesnopic <snesnopic@gmail.com>
2023-11-13 11:44:35 +0100

    Initial Commit

---
+++ FakeTelegram/FakeTelegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeTelegramApp.swift
+//  FakeTelegram
+//
+//  Created by Giuseppe Francione on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FakeTelegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
