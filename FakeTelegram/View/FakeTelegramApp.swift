7abe9af729f7a1b9f96ab370d78ad93e35c02a67
Snesnopic <snesnopic@gmail.com>
2023-11-14 14:04:24 +0100

    Now the badge actually displays how many unread messages there are

--- FakeTelegram/View/FakeTelegramApp.swift
+++ FakeTelegram/View/FakeTelegramApp.swift
@@ -13,7 +13,9 @@ struct FakeTelegramApp: App {
         WindowGroup{
             TabView{
                 ChatListView().badge("!").tabItem { Label("Contacts",systemImage: "person.crop.circle") }.background(.gray)
-                ChatListView().badge(2).tabItem { Label("Chat",systemImage: "bubble.left.and.bubble.right.fill") }
+                ChatListView().badge(chats.reduce(0) { result, chat in
+                    result + chat.unreadMessages
+                }).tabItem { Label("Chat",systemImage: "bubble.left.and.bubble.right.fill") }
                 ChatListView().tabItem { Label("Settings",systemImage: "gear") }
             }
         }
