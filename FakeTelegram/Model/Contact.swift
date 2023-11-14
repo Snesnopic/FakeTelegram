14c3c8741437689e295393e4eb893dd0c6adb718
Snesnopic <snesnopic@gmail.com>
2023-11-14 15:47:34 +0100

    image is of type Image now, and it's either a random image or the stock icon

--- FakeTelegram/Model/Contact.swift
+++ FakeTelegram/Model/Contact.swift
@@ -6,18 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
 
 class Contact: Identifiable{
     var unreadMessages:Int
     var chatType:ChatTypeEnum
     var name:String
-    var image:String?
+    var image:Image
     var messages:[Message]
-    init(unreadMessages: Int, chatType: ChatTypeEnum, name: String, image: String? = nil, messages: [Message]) {
+    init(unreadMessages: Int, chatType: ChatTypeEnum, name: String, messages: [Message]) {
         self.unreadMessages = unreadMessages
         self.chatType = chatType
         self.name = name
-        self.image = image
+        let index = Int.random(in:0...45)
+        if index < 24 {
+            if index < 10 {
+                self.image = Image("dog_000\(index)").resizable()
+            }
+            else {
+                self.image =  Image("dog_00\(index)").resizable()
+            }
+        }
+        else {
+            self.image = Image(systemName: chatType.defaultImage)
+        }
         self.messages = messages
     }
     func getLastMessage() -> Message {
