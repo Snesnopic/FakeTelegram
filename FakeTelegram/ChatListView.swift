b407148655974c5775886e3ca806734093414e63
Snesnopic <snesnopic@gmail.com>
2023-11-14 13:12:09 +0100

    No more folders, now chatlist is a List, fixed the search bar

--- FakeTelegram/ChatListView.swift
+++ FakeTelegram/ChatListView.swift
@@ -8,55 +8,72 @@
 import SwiftUI
 
 struct ChatListView: View {
-    var folders:[Folder] = [Folder(name: "Work colleagues", chats: [Contact(chatType: .personal, name: "Gianluca", sentMessages: [], receivedMessages: [])]),Folder(name: "Meme channels", chats: [Contact(chatType: .channel,name: "Memes", sentMessages: [], receivedMessages: [])]),Folder(name: "Database", chats: [Contact(chatType: .group,name: "Peeps from Secondigliano", sentMessages: [], receivedMessages: [])]),]
-    init(){
-        
-    }
+    var chats:[Contact] = [
+        Contact(chatType: .personal, name: "Gianluca", sentMessages: [Message(message: "Ciao", date: Date())], receivedMessages: [Message(message: "Ciao", date: Date())]),
+        Contact(chatType: .channel, name: "Meme channel", sentMessages: [Message(message: "Ciao", date: Date())], receivedMessages: [Message(message: "Mario Doccia", date: Date())]),
+        Contact(chatType: .group, name: "Gruppo uscite sabato", sentMessages: [Message(message: "Ciao", date: Date())], receivedMessages: [Message(message: "Alle 9 da Cibus", date: Date().addingTimeInterval(1))])]
+    
     @State private var selectedTab = 0
     @State private var searchText = ""
     var body: some View {
         NavigationStack{
-            VStack {
-                VStack{
-                    Color.clear.frame(height: 90)
-                    HStack{
-                        Image(systemName: "magnifyingglass")
-                        Text("Search")
-                    } .padding(.vertical, 10)
-                        .padding(.horizontal, 130)
-                        .background(Color.secondary.opacity(0.3))
-                        .clipShape(RoundedRectangle(cornerRadius: 10.0))
-                    
-                }.background(.gray.opacity(0.2)).ignoresSafeArea()
-                
-                
-                Spacer()
-            }
-            .toolbar {
-                ToolbarItem(placement: .principal){
+            List(chats.sorted(by: {
+                chat1, chat2 in
+                return chat1.getLastMessage().date > chat2.getLastMessage().date
+            })){
+                chat in
+                HStack(alignment: .top){
+                    Circle().stroke(.primary, lineWidth: 1)
+                        .frame(width: 70, height: 70)
+                        .overlay(Image(systemName: chat.image ?? chat.chatType.defaultImage).font(.system(size: 32)))
+                    VStack(alignment: .leading){
+                        Text(chat.name).bold()
+                        Text(chat.getLastMessage().message).opacity(0.8)
+                    }
+                    Spacer()
                     VStack {
-                        Text("Chat").bold()
+                        Text("\(chat.getLastMessage().getFormattedDate())")
                     }
                 }
-                ToolbarItem(placement: .topBarLeading)
-                {
+                .listRowInsets(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                .background(.background)
+                .swipeActions {
+                    Button(role: .destructive, action: {}, label: {
+                        VStack{
+                            Image(systemName: "trash.fill")
+                            Text("Delete")
+                        }})
                     Button(action: {}, label: {
-                        Text("Edit")
-                    })
+                        VStack{
+                            Image(systemName: "speaker.slash.fill")
+                            Text("Mute")
+                        }})
+                    
                 }
-                ToolbarItem(placement: .topBarTrailing)
-                {
-                    Button(action: {}, label: {
-                        Image(systemName: "plus.square.dashed")
-                    })
-                }
-                ToolbarItem(placement: .topBarTrailing)
-                {
-                    Button(action: {}, label: {
-                        Image(systemName: "square.and.pencil")
-                    })
-                }
-            }
+            }.listStyle(.plain)
+                .searchable(text: $searchText)
+                .navigationTitle("Chats")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading)
+                    {
+                        Button(action: {}, label: {
+                            Text("Edit")
+                        })
+                    }
+                    ToolbarItem(placement: .topBarTrailing)
+                    {
+                        Button(action: {}, label: {
+                            Image(systemName: "plus.square.dashed")
+                        })
+                    }
+                    ToolbarItem(placement: .topBarTrailing)
+                    {
+                        Button(action: {}, label: {
+                            Image(systemName: "square.and.pencil")
+                        })
+                    }
+                }.background(.foreground.opacity(0.03))
         }
     }
 }
