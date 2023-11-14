c77958c042e0f7bd4dea748206a29145fe2c9279
Snesnopic <snesnopic@gmail.com>
2023-11-14 13:33:13 +0100

    Since messages have a sender now, they don't need two arrays

--- FakeTelegram/Contact.swift
+++ FakeTelegram/Contact.swift
@@ -11,23 +11,20 @@ class Contact: Identifiable{
     var chatType:ChatTypeEnum
     var name:String
     var image:String?
-    var sentMessages:[Message]
-    var receivedMessages:[Message]
-    init(chatType: ChatTypeEnum, name: String, image: String? = nil, sentMessages: [Message], receivedMessages: [Message]) {
+    var messages:[Message]
+    init(chatType: ChatTypeEnum, name: String, image: String? = nil, messages: [Message]) {
         self.chatType = chatType
         self.name = name
         self.image = image
-        self.sentMessages = sentMessages
-        self.receivedMessages = receivedMessages
+        self.messages = messages
     }
     func getLastMessage() -> Message {
-        if(sentMessages.isEmpty && receivedMessages.isEmpty) {
-            return Message(message: "", date: Date())
+        if(messages.isEmpty) {
+            return Message(sender: "",message: "", date: Date())
         }
         else {
-            var allMessages = sentMessages + receivedMessages
-            allMessages.sort(){$0.date > $1.date}
-            return allMessages[0]
+            messages.sort(){$0.date > $1.date}
+            return messages[0]
         }
     }
 }
