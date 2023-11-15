801bf219aa257d4334f641b967912405197cc3f3
Snesnopic <snesnopic@gmail.com>
2023-11-15 14:12:53 +0100

    Added new component for the bubble shape

---
+++ FakeTelegram/View/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  FakeTelegram
+//
+//  Created by Giuseppe Francione on 15/11/23.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
