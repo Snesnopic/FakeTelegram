5cb1b21d64af421d6b275a9ece88cdc211002f0c
Snesnopic <snesnopic@gmail.com>
2023-11-15 15:04:04 +0100

    Now can display the name if provided (and if the sender isn't the user), background color for other sender is now white instead of gray

--- FakeTelegram/View/ContentMessageView.swift
+++ FakeTelegram/View/ContentMessageView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct ContentMessageView: View {
+    var sender:String?
     var messageContent:String
     var isCurrentUser:Bool
     var body: some View {
-        Text(messageContent)
-            .padding(10)
+        VStack (alignment: .leading){
+            if(sender != nil && sender != myself){
+                Text(sender!)
+            }
+            Text(messageContent)
+        }.padding(10)
             .foregroundColor(isCurrentUser ? Color.white : Color(UIColor.label))
-            .background(isCurrentUser ? Color.blue : Color(UIColor.label).opacity(0.2))
+            .background(isCurrentUser ? Color.blue : Color(UIColor.systemBackground))
     }
 }
 
