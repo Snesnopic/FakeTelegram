ddc48160c6a478b5bbe7577b4d06c975b2f5058b
Snesnopic <snesnopic@gmail.com>
2023-11-15 10:59:44 +0100

    Added new variable and new constructor

--- FakeTelegram/Model/Contact.swift
+++ FakeTelegram/Model/Contact.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 class Contact: Identifiable{
+    var seenByOther:Bool = false
     var unreadMessages:Int
     var chatType:ChatTypeEnum
     var name:String
@@ -31,6 +32,25 @@ class Contact: Identifiable{
             self.image = Image(systemName: chatType.defaultImage)
         }
         self.messages = messages
+    }
+    init(unreadMessages: Int, chatType: ChatTypeEnum, name: String, messages: [Message], seenByOther: Bool) {
+        self.unreadMessages = unreadMessages
+        self.chatType = chatType
+        self.name = name
+        let index = Int.random(in:0...45)
+        if index < 24 {
+            if index < 10 {
+                self.image = Image("dog_000\(index)").resizable()
+            }
+            else {
+                self.image =  Image("dog_00\(index)").resizable()
+            }
+        }
+        else {
+            self.image = Image(systemName: chatType.defaultImage)
+        }
+        self.messages = messages
+        self.seenByOther = seenByOther
     }
     func getLastMessage() -> Message {
         if(messages.isEmpty) {
