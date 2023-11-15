4b3da9e5e3c5261773908394f9165275214d1f1e
Snesnopic <snesnopic@gmail.com>
2023-11-15 15:55:47 +0100

    Now the default tab selected is the correct one

--- FakeTelegram/View/FakeTelegramApp.swift
+++ FakeTelegram/View/FakeTelegramApp.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 @main
 struct FakeTelegramApp: App {
+    @State private var selection:Int = 1
     var body: some Scene {
         WindowGroup{
-            TabView{
-                ChatListView().badge("!").tabItem { Label("Contacts",systemImage: "person.crop.circle") }.background(.gray)
+            TabView(selection: $selection){
+                ChatListView().badge("!").tabItem { Label("Contacts",systemImage: "person.crop.circle") }.background(.gray).tag(0)
                 ChatListView().badge(chats.reduce(0) { result, chat in
                     result + chat.unreadMessages
-                }).tabItem { Label("Chat",systemImage: "bubble.left.and.bubble.right.fill") }
-                ChatListView().tabItem { Label("Settings",systemImage: "gear") }
+                }).tabItem { Label("Chat",systemImage: "bubble.left.and.bubble.right.fill") }.tag(1)
+                ChatListView().tabItem { Label("Settings",systemImage: "gear") }.tag(2)
             }
         }
         
