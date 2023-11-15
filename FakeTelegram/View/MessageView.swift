7409564bbff08d0b138168f165df93f795186544
Snesnopic <snesnopic@gmail.com>
2023-11-15 14:14:25 +0100

    Added preset of the MessageView

--- FakeTelegram/View/MessageView.swift
+++ FakeTelegram/View/MessageView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct MessageView: View {
+    var image:Image?
+    var chatType:ChatTypeEnum
+    var currentMessage:Message
+    var isLastMessageInColumn:Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+            HStack(alignment: .bottom) {
+                if (currentMessage.sender == myself) {
+                    Spacer()
+                }
+                if(currentMessage.sender != myself) {
+                    if(image != nil){
+                        image!.resizable()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .clipShape(Circle())
+                    } else {
+                        Image(systemName: "plus").resizable()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .clipShape(Circle())
+                    }
+                }
+                    
+                ContentMessageView(messageContent: currentMessage.message,
+                                   isCurrentUser: currentMessage.sender == myself).clipShape(ChatBubbleShape(direction: currentMessage.sender == myself ? .right : .left))
+                
+                if(currentMessage.sender != myself){
+                    Spacer()
+                }
+                
+            }
+        }
 }
 
 #Preview {
-    MessageView()
+    MessageView(image:Image("dog_0001"),chatType: .personal,currentMessage: Message(sender: "myself", message: "Ascanio is a better name", date: Date()),isLastMessageInColumn: false)
 }
