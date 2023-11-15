375120d283ed95b798ebb818a363b81c91589b7b
Snesnopic <snesnopic@gmail.com>
2023-11-15 09:08:56 +0100

    Whitespace changes

--- FakeTelegram/View/ChatListView.swift
+++ FakeTelegram/View/ChatListView.swift
@@ -15,82 +15,77 @@ struct ChatListView: View {
     @State private var searchText = ""
     var body: some View {
         NavigationStack{
-                List(chats.filter{
-                    chat in
-                    return searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
-                    chat.name
-                        .lowercased()
-                        .contains(searchText.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
-                }.sorted(by: {
-                    chat1, chat2 in
-                    return chat1.getLastMessage().date > chat2.getLastMessage().date
-                })){
-                    chat in
-                    RectangularContactView(chat: chat)
-                        .listRowInsets(EdgeInsets(top: 12, leading: 10, bottom: 13, trailing: 10))
-                        .background(.background)
-                        .swipeActions(edge: .trailing) {
-                            if(chat.chatType != .personal){
-                                Button(action: {}, label: {
-                                    VStack{
-                                        Image(systemName: "archivebox.fill")
-                                        Text("Archive")
-                                    }
-                                })
-                            }
-                            Button(role: .destructive, action: {}, label: {
-                                VStack{
-                                    Image(systemName: "trash.fill")
-                                    Text("Delete")
-                                }})
+            List(chats.filter{
+                chat in
+                return searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+                chat.name
+                    .lowercased()
+                    .contains(searchText.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
+            }.sorted(by: {
+                chat1, chat2 in
+                return chat1.getLastMessage().date > chat2.getLastMessage().date
+            })){
+                chat in
+                RectangularContactView(chat: chat)
+                    .listRowInsets(EdgeInsets(top: 12, leading: 10, bottom: 13, trailing: 10))
+                    .background(.background)
+                    .swipeActions(edge: .trailing) {
+                        if(chat.chatType != .personal){
                             Button(action: {}, label: {
                                 VStack{
-                                    Image(systemName: "speaker.slash.fill")
-                                    Text("Mute").font(.footnote)
-                                }}).tint(.orange)
+                                    Image(systemName: "archivebox.fill")
+                                    Text("Archive")
+                                }
+                            })
                         }
-                        .swipeActions(edge: .leading){
-                            Button(action: {}, label: {
-                                VStack{
-                                    Image(systemName: "message.badge.filled.fill")
-                                    Text("Unread")
-                                }}).tint(.blue)
-                            Button(action: {}, label: {
-                                VStack{
-                                    Image(systemName: "pin.fill")
-                                    Text("Pin")
-                                }}).tint(.green)
-                            
-                        }
+                        Button(role: .destructive, action: {}, label: {
+                            VStack{
+                                Image(systemName: "trash.fill")
+                                Text("Delete")
+                            }})
+                        Button(action: {}, label: {
+                            VStack{
+                                Image(systemName: "speaker.slash.fill")
+                                Text("Mute").font(.footnote)
+                            }}).tint(.orange)
+                    }
+                    .swipeActions(edge: .leading){
+                        Button(action: {}, label: {
+                            VStack{
+                                Image(systemName: "message.badge.filled.fill")
+                                Text("Unread")
+                            }}).tint(.blue)
+                        Button(action: {}, label: {
+                            VStack{
+                                Image(systemName: "pin.fill")
+                                Text("Pin")
+                            }}).tint(.green)
+                    }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading){
+                    Button(action: {}, label: {
+                        Text("Edit")
+                    })
                 }
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading)
-                    {
-                        Button(action: {}, label: {
-                            Text("Edit")
-                        })
-                    }
-                    ToolbarItem(placement: .topBarTrailing)
-                    {
-                        Button(action: {}, label: {
-                            Image(systemName: "plus.square.dashed")
-                        })
-                    }
-                    ToolbarItem(placement: .topBarTrailing)
-                    {
-                        Button(action: {}, label: {
-                            Image(systemName: "square.and.pencil")
-                        })
-                    }
+                ToolbarItem(placement: .topBarTrailing){
+                    Button(action: {}, label: {
+                        Image(systemName: "plus.square.dashed")
+                    })
                 }
-                .listStyle(.plain)
-                .toolbarBackground(Color(UIColor.systemBackground).opacity(0.03), for: .navigationBar)
-                .toolbarBackground(.visible, for: .navigationBar)
-                .searchable(text: $searchText)
-                .navigationTitle("Chats")
-                .navigationBarTitleDisplayMode(.inline)
+                ToolbarItem(placement: .topBarTrailing){
+                    Button(action: {}, label: {
+                        Image(systemName: "square.and.pencil")
+                    })
+                }
+            }
+            .listStyle(.plain)
+            .toolbarBackground(Color(UIColor.systemBackground).opacity(0.03), for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .searchable(text: $searchText)
+            .navigationTitle("Chats")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        
     }
 }
 
