a4a7b670166e7a87840344958266dcba57ccccf7
Snesnopic <snesnopic@gmail.com>
2023-11-15 11:21:15 +0100

    Small changes, put the chats in a fixed height size so that swipeActions always appear with label

--- FakeTelegram/View/ChatListView.swift
+++ FakeTelegram/View/ChatListView.swift
@@ -6,29 +6,28 @@
 //
 
 import SwiftUI
+
 var chats:[Contact] = [
     Contact(unreadMessages: 2, chatType: .personal, name: "Gianluca", messages: [Message(sender: myself,message: "Ciao", date: Date())]),
     Contact(unreadMessages: 0, chatType: .channel, name: "Meme channel", messages: [Message(sender: "Claudio",message: "Mario Doccia", date: Date())]),
-    Contact(unreadMessages: 4, chatType: .group, name: "Gruppo uscite sabato", messages: [Message(sender: myself,message: "Ciao, come stai?", date: Date()),Message(sender: "Salvatore",message: "Alle 9 da Cibus", date: Date().addingTimeInterval(1))])]
+    Contact(unreadMessages: 4, chatType: .group, name: "Gruppo uscite sabato", messages: [Message(sender: myself,message: "Ciao, come stai?", date: Date()),Message(sender: "Salvatore",message: "Alle 9 da Cibus", date: Date().addingTimeInterval(1))],seenByOther: true)]
+
 struct ChatListView: View {
-    @State private var selectedTab = 0
     @State private var searchText = ""
     var body: some View {
         NavigationStack{
             List(chats.filter{
                 chat in
                 return searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
-                chat.name
-                    .lowercased()
-                    .contains(searchText.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
+                chat.name.lowercased().contains(searchText.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
             }.sorted(by: {
                 chat1, chat2 in
                 return chat1.getLastMessage().date > chat2.getLastMessage().date
             })){
                 chat in
                 RectangularContactView(chat: chat)
-                    .listRowInsets(EdgeInsets(top: 12, leading: 10, bottom: 13, trailing: 10))
-                    .background(.background)
+                    .listRowInsets(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                    .frame(height: 70)
                     .swipeActions(edge: .trailing) {
                         if(chat.chatType != .personal){
                             Button(action: {}, label: {
