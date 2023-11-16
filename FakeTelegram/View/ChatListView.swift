93d61b42a9df4c31d1ee9156a66e7a56662807fb
Snesnopic <snesnopic@gmail.com>
2023-11-16 20:22:02 +0100

    Corrected the ordering of the chats for the 100th time but they still won't get updated

--- FakeTelegram/View/ChatListView.swift
+++ FakeTelegram/View/ChatListView.swift
@@ -18,7 +18,7 @@ struct ChatListView: View {
 
         Chat(seenByOther: false, unreadMessages: 3, chatType: .group, contact:
                 Contact(name: "Uscite sabato"), messages: [Message(sender: myself, message: "Ciao, come stai?", date: Date()), Message(sender:
-                Contact(name: "Salvatore"), message: "Alle 9 da Cibus", date: Date().addingTimeInterval(1))])]
+                Contact(name: "Salvatore"), message: "Alle 9 da Cibus", date: Date())])]
 
     @State private var searchText = ""
     var body: some View {
@@ -27,7 +27,7 @@ struct ChatListView: View {
                 return searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
                 chat.contact.name.lowercased().contains(searchText.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
             }.sorted(by: { chat1, chat2 in
-                return chat1.getLastMessage()!.date < chat2.getLastMessage()!.date
+                return chat1.getLastMessage()!.date > chat2.getLastMessage()!.date
             })) { chat in
                 NavigationLink {
                     ChatView(chat: chat)
