243bca5f00efe64c9e0e55f65af75c704dec21ce
Snesnopic <snesnopic@gmail.com>
2023-11-16 20:08:38 +0100

    You can actually send messages now! Reduced space between messages, applied the background to the frame and not the list, fixed the ordering

--- FakeTelegram/View/ChatView.swift
+++ FakeTelegram/View/ChatView.swift
@@ -8,31 +8,43 @@
 import SwiftUI
 
 struct ChatView: View {
-    var chat: Chat
+    @State var chat: Chat
     @State private var typingMessage: String = ""
     var body: some View {
         NavigationStack {
             VStack {
-                List(chat.messages.indices, id: \.self) { index in
-                    MessageView(chatType: chat.chatType,
-                        currentMessage: chat.messages[index],
-                        isLastMessageInColumn: isLastMessageInColumn(index:
-                        index)).listRowSeparator(.hidden).listRowBackground(Color.clear)
+                List {
+                    ForEach(chat.messages.sorted(by: { chat1, chat2 in
+                        return chat1.date > chat2.date
+                    }).indices, id: \.self) { index in
+                        MessageView(chatType: chat.chatType,
+                                    currentMessage: chat.messages[index],
+                                    isLastMessageInColumn: isLastMessageInColumn(index:
+                                    index))
+                        .listRowInsets(EdgeInsets(.zero))
+                        .listRowSeparator(.hidden)
+                        .listRowBackground(Color.clear)
+                    }
                 }.listStyle(.inset)
                     .scrollContentBackground(.hidden)
-                    .background(Image("background"))
+
                 HStack {
                     Image(systemName: "paperclip")
                     TextField("Message...", text: $typingMessage)
                         .textFieldStyle(.roundedBorder)
                         .frame(minHeight: 30)
-                    Button(action: {}) {
+                    Button(action: {
+                        if !typingMessage.isEmpty {
+                            chat.messages.append(Message(sender: myself, message: typingMessage, date: Date()))
+                            typingMessage = ""
+                        }
+                    }) {
                         Image(systemName: "arrow.up.circle.fill")
                             .resizable()
                             .frame(width: 30, height: 30)
                     }
                 }.padding().background(Color(UIColor.systemBackground).opacity(0.8))
-            }.navigationTitle(chat.contact.name)
+            }.background(Image("background")).navigationTitle(chat.contact.name)
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar(.hidden, for: .tabBar)
                 .toolbarBackground(Color(UIColor.systemBackground).opacity(0.03), for: .navigationBar)
@@ -53,6 +65,7 @@ struct ChatView: View {
 }
 
 #Preview {
-    let messages = [Message(sender: Contact(name: "Gianluca"), message: "Ciao", date: Date()), Message(sender: myself, message: "Ciao", date: Date())]
+    let messages = [Message(sender: Contact(name: "Gianluca"), message: "Questa è una risposta",
+    date: Date().addingTimeInterval(4)), Message(sender: myself, message: "Ciao!", date: Date().addingTimeInterval(2))]
     return ChatView(chat: Chat(seenByOther: true, unreadMessages: 2, chatType: .group, contact: Contact(name: "Gianluca"), messages: messages))
 }
